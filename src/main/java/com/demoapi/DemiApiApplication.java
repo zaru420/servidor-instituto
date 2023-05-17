0525b60d199e41e1bc94baba5d201be49c4dfc6d
jlasolis <jorge@clicaonline.com>
2023-05-17 14:14:52 +0200

    Primer Commit, recuerda que hemos cambiado en la BBDD los nombres a minuscula, y que para hacer la insercion debemis usar un countrycode que exista

---
+++ src/main/java/com/demoapi/DemiApiApplication.java
@@ -0,0 +1,16 @@
+package com.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class DemiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoapi/DemiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
