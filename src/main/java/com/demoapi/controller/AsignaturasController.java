831f5d0d84c45edc4a7216f30c9de3f8326464b0
Aaron <aaronzaru@gmail.com>
2023-05-22 10:02:43 +0200

    agregado el corss

--- src/main/java/com/demoapi/controller/AsignaturasController.java
+++ src/main/java/com/demoapi/controller/AsignaturasController.java
@@ -3,6 +3,7 @@ package com.demoapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.demoapi.model.Asignaturas;
 import com.demoapi.respository.AsignaturasRepository;
 
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class AsignaturasController {
--- src/main/java/com/demoapi/controller/TipoUsuarioController.java
+++ src/main/java/com/demoapi/controller/TipoUsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.demoapi.model.TipoUsuario;
 import com.demoapi.respository.TipoUsuarioRepository;
 
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class TipoUsuarioController {
